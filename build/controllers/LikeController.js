4942566cc3f57f69977a36c5ba7ea425e87c358a
gurpreetkaur <gurpreetkaur_batra@yahoo.in>
2022-04-02 23:47:02 -0400

    changing methods of likes

--- build/controllers/LikeController.js
+++ build/controllers/LikeController.js
@@ -1,23 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const LikeDao_1 = __importDefault(require("../daos/LikeDao"));
+const TuitDao_1 = __importDefault(require("../daos/TuitDao"));
 /**
- * @class TuitController Implements RESTful Web service API for likes resource.
+ * @class LikeController Implements RESTful Web service API for likes resource.
  * Defines the following HTTP endpoints:
  * <ul>
  *     <li>GET /api/users/:uid/likes to retrieve all the tuits liked by a user
  *     </li>
  *     <li>GET /api/tuits/:tid/likes to retrieve all users that liked a tuit
  *     </li>
- *     <li>POST /api/users/:uid/likes/:tid to record that a user likes a tuit
+ *     <li>GET /api/users/:uid/likes/:tid to record if a user likes a tuit
  *     </li>
- *     <li>DELETE /api/users/:uid/unlikes/:tid to record that a user
- *     no londer likes a tuit</li>
+ *     <li>PUT /api/users/:uid/likes/:tid to toggle that a user likes a tuit
+ *     </li>
  * </ul>
  * @property {LikeDao} likeDao Singleton DAO implementing likes CRUD operations
+ * @property {TuitDao} tuitDao Singleton DAO implementing tuits CRUD operations
  * @property {LikeController} LikeController Singleton controller implementing
  * RESTful Web service API
  */
@@ -39,8 +50,38 @@ class LikeController {
          * @param {Response} res Represents response to client, including the
          * body formatted as JSON arrays containing the tuit objects that were liked
          */
-        this.findAllTuitsLikedByUser = (req, res) => LikeController.likeDao.findAllTuitsLikedByUser(req.params.uid)
-            .then(likes => res.json(likes));
+        this.findAllTuitsLikedByUser = (req, res) => {
+            const uid = req.params.uid;
+            // @ts-ignore
+            const profile = req.session['profile'];
+            const userId = uid === 'me' && profile ?
+                profile._id : uid;
+            LikeController.likeDao.findAllTuitsLikedByUser(userId)
+                .then(likes => {
+                // filter out likes with null tuits. Only keep defined tuits
+                // extract tuit object from likes respond with tuits
+                const likesNonNullTuits = likes.filter(like => like.tuit);
+                const tuitsFromLikes = likesNonNullTuits.map(like => like.tuit);
+                res.json(tuitsFromLikes);
+            });
+        };
+        /**
+         * Check if the user has already liked the tuit
+         * @param {Request} req Represents request from client, including the path
+         * parameter uid representing the user, and the tid representing the tuit
+         * @param {Response} res Represents response to client, including the
+         * body formatted as JSON object containing the like objects or null
+         */
+        this.findUserLikedTuit = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const uid = req.params.uid;
+            const tid = req.params.tid;
+            // @ts-ignore
+            const profile = req.session['profile'];
+            const userId = uid === 'me' && profile ?
+                profile._id : uid;
+            LikeController.likeDao.findUserLikesTuit(userId, tid)
+                .then(like => res.json(like));
+        });
         /**
          * @param {Request} req Represents request from client, including the
          * path parameters uid and tid representing the user that is liking the tuit
@@ -49,35 +90,53 @@ class LikeController {
          * body formatted as JSON containing the new likes that was inserted in the
          * database
          */
-        this.userLikesTuit = (req, res) => LikeController.likeDao.userLikesTuit(req.params.uid, req.params.tid)
-            .then(likes => res.json(likes));
-        /**
-         * @param {Request} req Represents request from client, including the
-         * path parameters uid and tid representing the user that is unliking
-         * the tuit and the tuit being unliked
-         * @param {Response} res Represents response to client, including status
-         * on whether deleting the like was successful or not
-         */
-        this.userUnlikesTuit = (req, res) => LikeController.likeDao.userUnlikesTuit(req.params.uid, req.params.tid)
-            .then(status => res.send(status));
+        this.userTogglesTuitLikes = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const likeDao = LikeController.likeDao;
+            const tuitDao = LikeController.tuitDao;
+            const uid = req.params.uid;
+            const tid = req.params.tid;
+            // @ts-ignore
+            const profile = req.session['profile'];
+            const userId = uid === 'me' && profile ?
+                profile._id : uid;
+            try {
+                const userAlreadyLikedTuit = yield likeDao.findUserLikesTuit(userId, tid);
+                const howManyLikedTuit = yield likeDao.countHowManyLikedTuit(tid);
+                let tuit = yield tuitDao.findTuitById(tid);
+                if (userAlreadyLikedTuit) {
+                    yield likeDao.userUnlikesTuit(userId, tid);
+                    tuit.stats.likes = howManyLikedTuit - 1;
+                }
+                else {
+                    yield LikeController.likeDao.userLikesTuit(userId, tid);
+                    tuit.stats.likes = howManyLikedTuit + 1;
+                }
+                yield tuitDao.updateLikes(tid, tuit.stats);
+                res.sendStatus(200);
+            }
+            catch (e) {
+                res.sendStatus(404);
+            }
+        });
     }
 }
 exports.default = LikeController;
 LikeController.likeDao = LikeDao_1.default.getInstance();
+LikeController.tuitDao = TuitDao_1.default.getInstance();
 LikeController.likeController = null;
 /**
  * Creates singleton controller instance
  * @param {Express} app Express instance to declare the RESTful Web service
  * API
- * @return TuitController
+ * @return LikeController
  */
 LikeController.getInstance = (app) => {
     if (LikeController.likeController === null) {
         LikeController.likeController = new LikeController();
         app.get("/api/users/:uid/likes", LikeController.likeController.findAllTuitsLikedByUser);
         app.get("/api/tuits/:tid/likes", LikeController.likeController.findAllUsersThatLikedTuit);
-        app.post("/api/users/:uid/likes/:tid", LikeController.likeController.userLikesTuit);
-        app.delete("/api/users/:uid/unlikes/:tid", LikeController.likeController.userUnlikesTuit);
+        app.get("/api/users/:uid/likes/:tid", LikeController.likeController.findUserLikedTuit);
+        app.put("/api/users/:uid/likes/:tid", LikeController.likeController.userTogglesTuitLikes);
     }
     return LikeController.likeController;
 };
--- build/daos/TuitDao.js
+++ build/daos/TuitDao.js
@@ -18,38 +18,90 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const TuitModel_1 = __importDefault(require("../mongoose/tuits/TuitModel"));
 /**
- * @class UserDao Implements Data Access Object managing data storage
- * of Users
- * @property {UserDao} userDao Private single instance of UserDao
+ * @class TuitDao Implements Data Access Object managing data storage
+ * of Tuits
+ * @property {TuitDao} tuitDao Private single instance of TuitDao
  */
 class TuitDao {
     constructor() {
-        this.findAllTuits = () => __awaiter(this, void 0, void 0, function* () {
-            return TuitModel_1.default.find()
-                .populate("postedBy")
-                .exec();
-        });
+        /**
+         * Uses TuitModel to retrieve all tuit documents from tuits collection
+         * @returns Promise To be notified when the tuits are retrieved from
+         * database
+         */
+        this.findAllTuits = () => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.find(); });
+        /**
+         * Uses TuitModel to retrieve all tuit document from tuits collection
+         * @param {string} uid User's primary key
+         * @returns Promise To be notified when tuit is retrieved from the database
+         */
         this.findAllTuitsByUser = (uid) => __awaiter(this, void 0, void 0, function* () {
             return TuitModel_1.default.find({ postedBy: uid })
                 .sort({ 'postedOn': -1 })
                 .populate("postedBy")
                 .exec();
         });
+        /**
+         * Uses TuitModel to retrieve single tuit document from tuits collection
+         * @param {string} uid User's primary key
+         * @returns Promise To be notified when tuit is retrieved from the database
+         */
         this.findTuitById = (uid) => __awaiter(this, void 0, void 0, function* () {
             return TuitModel_1.default.findById(uid)
                 .populate("postedBy")
                 .exec();
         });
+        /**
+         * Inserts tuit instance into the database
+         * @param {User} uid Primary key of user that posts the tuit
+         * @param {Tuit} tuit Instance to be inserted into the database
+         * @returns Promise To be notified when tuit is inserted into the database
+         */
         this.createTuitByUser = (uid, tuit) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.create(Object.assign(Object.assign({}, tuit), { postedBy: uid })); });
+        /**
+         * Updates tuit with new values in database
+         * @param {string} uid Primary key of user to be modified
+         * @param {Tuit} tuit Tuit object containing properties and their new values
+         * @returns Promise To be notified when tuit is updated in the database
+         */
         this.updateTuit = (uid, tuit) => __awaiter(this, void 0, void 0, function* () {
             return TuitModel_1.default.updateOne({ _id: uid }, { $set: tuit });
         });
+        /**
+         * Updates likes count with new values in database
+         * @param {string} tid Primary key of tuit stas to be modified
+         * @param {any} newStats new stats object for the tuit to be updated
+         * @returns Promise To be notified when tuit stats is updated in the database
+         */
+        this.updateLikes = (tid, newStats) => __awaiter(this, void 0, void 0, function* () {
+            return TuitModel_1.default.updateOne({ _id: tid }, { $set: { stats: newStats } });
+        });
+        /**
+         * Removes tuit from the database.
+         * @param {string} uid Primary key of user tuit to be removed
+         * @returns Promise To be notified when user tuit is removed from the database
+         */
         this.deleteTuit = (uid) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.deleteOne({ _id: uid }); });
+        /**
+         * Removes tuit from the database.
+         * @param {string} tid Primary key of tuit to be removed
+         * @returns Promise To be notified when user tuit is removed from the database
+         */
+        this.deleteTuitById = (tid) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.deleteOne({ _id: tid }); });
+        /**
+         * Removes tuit from the database.
+         * @param {string} tuit content of the tuit to be removed
+         * @returns Promise To be notified when user tuit is removed from the database
+         */
         this.deleteTuitByContent = (tuit) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.deleteMany({ tuit }); });
     }
 }
 exports.default = TuitDao;
 TuitDao.tuitDao = null;
+/**
+ * Creates singleton DAO instance
+ * @returns TuitDao
+ */
 TuitDao.getInstance = () => {
     if (TuitDao.tuitDao === null) {
         TuitDao.tuitDao = new TuitDao();
